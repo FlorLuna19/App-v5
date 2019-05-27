4ee06221060cd7fabf0cd5ddf58e3f12ef4550b5
Florencia <floppy_luna@hotmail.com>
2019-05-27 17:36:48 -0300

    usuario

--- server/server.js
+++ server/server.js
@@ -68,39 +68,104 @@ app.set("views", path.join(__dirname, "views"));
 
 //GET
 //Ingreso a la carpeta raíz
-app.get('/', function (req, res) {
-  console.log("Entre al login");
-  res.sendFile(path.join(__dirname, '../client/html/index.html'));
-})
-
-//Ingreso a home con Handlebars
-app.get("/home", function(req, res) {
+app.get("/", function(req, res) {
   console.log("Entre al home con handlebars")
-  res.render("home");
+
+  if(req.session.userId !== undefined) {
+    const datosUsuarioLogueado = {
+      nombre: req.session.userNombre,
+      email: req.session.email,
+      residencia: req.session.residencia
+        }
+//Esa es la vista del usuario logueado
+      res.render("home", {
+      data: datosUsuarioLogueado
+      
+      });
+
+      } else {
+        //Es la vista de usuario no logueado
+        res.render('home')
+      }
+
 });
 
-//Ingreso a usuario
-//app.get("/usuario", function(req, res) {
-  //console.log("Entre a usuario con handlebars")
-  //res.render("usuario");
-//});
+//GET
+//Ingreso a la login
+app.get('/login', function (req, res) {
+  console.log("Entre al login");
+  res.sendFile(path.join(__dirname, '../client/html/login.html'));
+})
+
+
 
 //Ingreso a recorrido
 app.get("/recorrido", function(req, res) {
   console.log("Entre a recorrido con handlebars")
-  res.render("recorrido");
+
+  if(req.session.userId !== undefined) {
+    const datosUsuarioLogueado = {
+      nombre: req.session.userNombre,
+      email: req.session.email,
+      residencia: req.session.residencia
+        }
+//Esa es la vista del usuario logueado
+      res.render("recorrido", {
+      data: datosUsuarioLogueado
+      
+      });
+
+      } else {
+        //Es la vista de usuario no logueado
+        res.render('recorrido')
+      }
+
 });
 
 //Ingreso a empresa
 app.get("/empresa", function(req, res) {
   console.log("Entre a empresa con handlebars")
-  res.render("empresa");
+
+  if(req.session.userId !== undefined) {
+    const datosUsuarioLogueado = {
+      nombre: req.session.userNombre,
+      email: req.session.email,
+      residencia: req.session.residencia
+        }
+//Esa es la vista del usuario logueado
+      res.render("empresa", {
+      data: datosUsuarioLogueado
+      
+      });
+
+      } else {
+        //Es la vista de usuario no logueado
+        res.render('empresa')
+      }
+  
 });
 
 //Ingreso a calendario
 app.get("/calendario", function(req, res) {
   console.log("Entre a calendario con handlebars")
-  res.render("calendario");
+
+  if(req.session.userId !== undefined) {
+    const datosUsuarioLogueado = {
+      nombre: req.session.userNombre,
+      email: req.session.email,
+      residencia: req.session.residencia
+        }
+//Esa es la vista del usuario logueado
+      res.render("calendario", {
+      data: datosUsuarioLogueado
+      
+      });
+
+      } else {
+        //Es la vista de usuario no logueado
+        res.render('calendario')
+      }
+  
 });
 
 //Ingreso a reserva con Mongodb
@@ -125,12 +190,34 @@ app.get("/reserva", function(req, res) {
 
 
     coleccionIda.find().toArray(function(err, datosIda) {
-      //console.log(data);
-      res.render("reserva", {
-        //Asigno una etiqueta 'datosLugares' para usar los datos del array que traigo de la colección
+      
+    //console.log(data);
+    if(req.session.userId !== undefined) {
+      const datosUsuarioLogueado = {
+        nombre: req.session.userNombre,
+        email: req.session.email,
+        residencia: req.session.residencia
+          }
+  //Esa es la vista del usuario logueado
+        res.render("reserva", {
+        data: datosUsuarioLogueado,
         datosIda: datosIda,
         datosVuelta: datosVuelta
-      });
+        
+        });
+  
+        } else {
+          //Es la vista de usuario no logueado
+          res.render("reserva", {
+            //Asigno una etiqueta 'datosLugares' para usar los datos del array que traigo de la colección
+            datosIda: datosIda,
+            datosVuelta: datosVuelta
+          });
+        }
+
+
+
+      
 
     });
 
@@ -142,25 +229,24 @@ app.get("/reserva", function(req, res) {
 
 //Ingreso a usuario con Mongodb
 app.get("/usuario", function(req, res) {
-  // Conectamos a MongoDB                      
-  client.connect(function(error, client) {
+  
+  if(req.session.userId !== undefined) {
+    const datosUsuarioLogueado = {
+      nombre: req.session.userNombre,
+      email: req.session.email,
+      residencia: req.session.residencia
+        }
 
-    // Insertamos la base de datos que usaremos
-    const db = client.db("expressdb");
-
-    // Especificamos que coleccion usaremos
-    let coleccion = db.collection("usuarios");
-    console.log(coleccion);
-
-    coleccion.find().toArray(function(err, data) {
-      console.log(data);
       res.render("usuario", {
-      data: data
+          data: datosUsuarioLogueado
       });
 
+      } else {
+        res.redirect('/registro.html')
+      }
+ 
     });
-  });
-});
+
 
 
 
@@ -175,18 +261,24 @@ client.connect(function(error, client) {
   // Insertamos la base de datos que usaremos
   const db = client.db("expressdb");
 
-
   let coleccionUsuarios = db.collection("usuarios");
 
   coleccionUsuarios.find({nombre: nombre, password: password}).toArray(function(err, datosLogin) {
     console.log(datosLogin);
 
     if(datosLogin.length == 1) {
-    console.log("estoy logueado")
-    res.redirect('/home');
+    console.log("Estoy logueado");
+
+    req.session.userId = datosLogin[0]._id;
+    req.session.userNombre = datosLogin[0].nombre;
+    req.session.email = datosLogin[0].email;
+    req.session.residencia = datosLogin[0].residencia;
+
+    res.redirect('/');
+
     }else{
-      res.redirect('/index.html')
-      console.log("no me loguee")
+      res.redirect('/login')
+      console.log("No me loguee")
     }
     
 
@@ -196,6 +288,39 @@ client.connect(function(error, client) {
 });
 
 
+//GET logout
+app.get('/logout', (req, res) => {
+  //Sale de sesión y redirijo al login
+  req.session.destroy();
+  res.redirect("/");
+})
+
+
+
+
+//Ruta para registro
+app.post('/registro/form', (req, res) => {
+  
+  const usuarioNuevo = {
+    nombre: req.body.user,
+    email: req.body.email,
+    residencia: req.body.residencia,
+    password: req.body.password
+  }
+  console.log(usuarioNuevo)
+
+  client.connect(function(err, client) {
+    const db = client.db("expressdb");
+
+    let coleccionUsuarioNuevo = db.collection("usuarios");
+
+    coleccionUsuarioNuevo.insertOne(usuarioNuevo, (err, datoCreado) => {
+      res.redirect('/index.html')
+    })
+
+  })
+
+} )
 
 
 
